abc20ac7f40f5683bca69b2b36e375921a4c016d
AS <ahmedselo2005@gmail.com>
2024-08-11 21:58:51 +0300

    Add a custom UserDetailsService

---
+++ authorization-server/src/main/java/com/springbite/authorization_server/services/CustomUserDetailsService.java
@@ -0,0 +1,26 @@
+package com.springbite.authorization_server.services;
+
+import com.springbite.authorization_server.model.SecurityUser;
+import com.springbite.authorization_server.model.User;
+import com.springbite.authorization_server.repositories.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public CustomUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+        return new SecurityUser(user);
+    }
+}
