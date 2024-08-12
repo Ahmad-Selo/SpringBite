475c65c0d51086c8d4e7afe2f85eaf8ce885cb43
AS <ahmedselo2005@gmail.com>
2024-08-12 16:34:30 +0300

    Add a mapper class for RegisteredClient and rename RegisteredClientService to CustomRegisteredClientRepository and move it to repositories package

---
+++ authorization-server/src/main/java/com/springbite/authorization_server/repositories/CustomRegisteredClientRepository.java
@@ -0,0 +1,40 @@
+package com.springbite.authorization_server.repositories;
+
+import com.springbite.authorization_server.entities.RegisteredClientEntity;
+import com.springbite.authorization_server.mappers.RegisteredClientMapper;
+import org.springframework.security.oauth2.server.authorization.client.RegisteredClient;
+import org.springframework.security.oauth2.server.authorization.client.RegisteredClientRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomRegisteredClientRepository implements RegisteredClientRepository {
+
+    private final JpaRegisteredClientRepository jpaRegisteredClientRepository;
+
+    private final RegisteredClientMapper registeredClientMapper;
+
+    public CustomRegisteredClientRepository(JpaRegisteredClientRepository registeredClientRepository, RegisteredClientMapper registeredClientMapper) {
+        this.jpaRegisteredClientRepository = registeredClientRepository;
+        this.registeredClientMapper = registeredClientMapper;
+    }
+
+    @Override
+    public void save(RegisteredClient registeredClient) {
+        RegisteredClientEntity entity = registeredClientMapper.toEntity(registeredClient);
+        jpaRegisteredClientRepository.save(entity);
+    }
+
+    @Override
+    public RegisteredClient findById(String id) {
+        return jpaRegisteredClientRepository.findById(Integer.valueOf(id))
+                .map(registeredClientMapper::toRegisteredClient)
+                .orElse(null);
+    }
+
+    @Override
+    public RegisteredClient findByClientId(String clientId) {
+        return jpaRegisteredClientRepository.findByClientId(clientId)
+                .map(registeredClientMapper::toRegisteredClient)
+                .orElse(null);
+    }
+}
