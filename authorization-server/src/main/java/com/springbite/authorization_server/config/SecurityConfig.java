982e0e64a957c876c464c4bd4737f256ee5a4503
AS <ahmedselo2005@gmail.com>
2024-08-12 16:11:55 +0300

    Add authorities claim to jwt

--- authorization-server/src/main/java/com/springbite/authorization_server/config/SecurityConfig.java
+++ authorization-server/src/main/java/com/springbite/authorization_server/config/SecurityConfig.java
@@ -7,23 +7,23 @@ import com.nimbusds.jose.jwk.source.JWKSource;
 import com.nimbusds.jose.proc.SecurityContext;
 import com.springbite.authorization_server.entities.KeyPairEntity;
 import com.springbite.authorization_server.repositories.KeyPairRepository;
-import com.springbite.authorization_server.services.CustomUserDetailsService;
-import com.springbite.authorization_server.services.RegisteredClientService;
 import com.springbite.authorization_server.utils.KeyPairUtil;
 import jakarta.transaction.Transactional;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.oauth2.server.authorization.client.RegisteredClientRepository;
+import org.springframework.security.oauth2.jwt.JwtClaimsSet;
 import org.springframework.security.oauth2.server.authorization.config.annotation.web.configuration.OAuth2AuthorizationServerConfiguration;
 import org.springframework.security.oauth2.server.authorization.config.annotation.web.configurers.OAuth2AuthorizationServerConfigurer;
 import org.springframework.security.oauth2.server.authorization.settings.AuthorizationServerSettings;
+import org.springframework.security.oauth2.server.authorization.token.JwtEncodingContext;
+import org.springframework.security.oauth2.server.authorization.token.OAuth2TokenCustomizer;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint;
 
@@ -34,6 +34,7 @@ import java.security.interfaces.RSAPrivateKey;
 import java.security.interfaces.RSAPublicKey;
 import java.security.spec.InvalidKeySpecException;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Configuration
 public class SecurityConfig {
@@ -119,5 +120,17 @@ public class SecurityConfig {
     @Bean
     public AuthorizationServerSettings authorizationServerSettings() {
         return AuthorizationServerSettings.builder().build();
+    }
+
+    @Bean
+    public OAuth2TokenCustomizer<JwtEncodingContext> jwtCustomizer() {
+        return context -> {
+            Authentication authentication = context.getPrincipal();
+            JwtClaimsSet.Builder claims = context.getClaims();
+            claims.claim("authorities",
+                    authentication.getAuthorities().stream()
+                            .map(GrantedAuthority::getAuthority)
+                            .collect(Collectors.toList()));
+        };
     }
 }
