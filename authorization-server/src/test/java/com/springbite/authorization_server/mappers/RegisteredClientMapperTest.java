76a160054f64199662fcfc45c42cca48b632677e
AS <ahmedselo2005@gmail.com>
2024-08-13 11:08:54 +0300

    Add a tests for RegisteredClientMapper

---
+++ authorization-server/src/test/java/com/springbite/authorization_server/mappers/RegisteredClientMapperTest.java
@@ -0,0 +1,123 @@
+package com.springbite.authorization_server.mappers;
+
+import com.springbite.authorization_server.entities.RegisteredClientEntity;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.security.oauth2.core.AuthorizationGrantType;
+import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
+import org.springframework.security.oauth2.core.oidc.OidcScopes;
+import org.springframework.security.oauth2.server.authorization.client.RegisteredClient;
+
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class RegisteredClientMapperTest {
+
+    private RegisteredClientMapper registeredClientMapper;
+
+    @BeforeEach
+    void setUp() {
+        this.registeredClientMapper = new RegisteredClientMapper();
+    }
+
+    @Test
+    void RegisteredClientToRegisteredClientEntityTest() {
+        RegisteredClient registeredClient =
+                RegisteredClient.withId("1")
+                        .clientId("client")
+                        .clientSecret("secret")
+                        .clientAuthenticationMethod(
+                                ClientAuthenticationMethod.CLIENT_SECRET_BASIC
+                        )
+                        .authorizationGrantType(
+                                AuthorizationGrantType.AUTHORIZATION_CODE
+                        )
+                        .redirectUri(
+                                "http://localhost:8080/login/oauth2/code/springbite"
+                        )
+                        .scope(OidcScopes.OPENID)
+                        .build();
+
+        RegisteredClientEntity registeredClientEntity = registeredClientMapper
+                .toRegisteredClientEntity(registeredClient);
+
+        assertEquals(registeredClient.getClientId(), registeredClientEntity.getClientId());
+
+        assertEquals(registeredClient.getClientSecret(), registeredClientEntity.getClientSecret());
+
+        assertEquals(
+                registeredClient.getClientAuthenticationMethods()
+                        .stream()
+                        .map(ClientAuthenticationMethod::getValue)
+                        .collect(Collectors.toSet()),
+                registeredClientEntity.getClientAuthenticationMethods()
+        );
+
+        assertEquals(
+                registeredClient.getAuthorizationGrantTypes()
+                        .stream()
+                        .map(AuthorizationGrantType::getValue)
+                        .collect(Collectors.toSet()),
+                registeredClientEntity.getAuthorizationGrantTypes()
+        );
+
+        assertEquals(
+                registeredClient.getRedirectUris(),
+                registeredClientEntity.getRedirectUris()
+        );
+
+        assertEquals(
+                registeredClient.getScopes(),
+                registeredClientEntity.getScopes()
+        );
+    }
+
+    @Test
+    void RegisteredClientEntityToRegisteredClientTest() {
+        RegisteredClientEntity registeredClientEntity =
+                new RegisteredClientEntity(
+                        "client",
+                        "secret",
+                        Set.of("client_secret_basic"),
+                        Set.of("authorization_code"),
+                        Set.of("http://localhost:8080/login/oauth2/code/springbite"),
+                        Set.of("openid")
+                );
+
+        RegisteredClient registeredClient = registeredClientMapper
+                .toRegisteredClient(registeredClientEntity);
+
+        assertEquals(registeredClientEntity.getClientId(), registeredClient.getClientId());
+
+        assertEquals(registeredClientEntity.getClientSecret(), registeredClient.getClientSecret());
+
+        assertEquals(
+                registeredClientEntity.getClientAuthenticationMethods(),
+                registeredClient.getClientAuthenticationMethods()
+                        .stream()
+                        .map(ClientAuthenticationMethod::getValue)
+                        .collect(Collectors.toSet())
+        );
+
+        assertEquals(
+                registeredClientEntity.getAuthorizationGrantTypes(),
+                registeredClient.getAuthorizationGrantTypes()
+                        .stream()
+                        .map(AuthorizationGrantType::getValue)
+                        .collect(Collectors.toSet())
+        );
+
+        assertEquals(
+                registeredClientEntity.getRedirectUris(),
+                registeredClient.getRedirectUris()
+        );
+
+        assertEquals(
+                registeredClientEntity.getScopes(),
+                registeredClient.getScopes()
+        );
+
+    }
+}
