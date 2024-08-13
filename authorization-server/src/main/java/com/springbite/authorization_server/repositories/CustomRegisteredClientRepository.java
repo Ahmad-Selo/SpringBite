e3b8014f0070967f9f7430f1e434a0bb304f8484
AS <ahmedselo2005@gmail.com>
2024-08-13 17:26:37 +0300

    Rename CustomRegisteredClientRepository to RegisteredClientRepositoryImpl and JpaRegisteredClientRepository to CustomRegisteredClientRepository,

--- authorization-server/src/main/java/com/springbite/authorization_server/repositories/CustomRegisteredClientRepository.java
+++ authorization-server/src/main/java/com/springbite/authorization_server/repositories/CustomRegisteredClientRepository.java
@@ -1,40 +1,10 @@
 package com.springbite.authorization_server.repositories;
 
 import com.springbite.authorization_server.entities.RegisteredClientEntity;
-import com.springbite.authorization_server.mappers.RegisteredClientMapper;
-import org.springframework.security.oauth2.server.authorization.client.RegisteredClient;
-import org.springframework.security.oauth2.server.authorization.client.RegisteredClientRepository;
-import org.springframework.stereotype.Service;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-@Service
-public class CustomRegisteredClientRepository implements RegisteredClientRepository {
+import java.util.Optional;
 
-    private final JpaRegisteredClientRepository jpaRegisteredClientRepository;
-
-    private final RegisteredClientMapper registeredClientMapper;
-
-    public CustomRegisteredClientRepository(JpaRegisteredClientRepository registeredClientRepository, RegisteredClientMapper registeredClientMapper) {
-        this.jpaRegisteredClientRepository = registeredClientRepository;
-        this.registeredClientMapper = registeredClientMapper;
-    }
-
-    @Override
-    public void save(RegisteredClient registeredClient) {
-        RegisteredClientEntity entity = registeredClientMapper.toRegisteredClientEntity(registeredClient);
-        jpaRegisteredClientRepository.save(entity);
-    }
-
-    @Override
-    public RegisteredClient findById(String id) {
-        return jpaRegisteredClientRepository.findById(Long.valueOf(id))
-                .map(registeredClientMapper::toRegisteredClient)
-                .orElse(null);
-    }
-
-    @Override
-    public RegisteredClient findByClientId(String clientId) {
-        return jpaRegisteredClientRepository.findByClientId(clientId)
-                .map(registeredClientMapper::toRegisteredClient)
-                .orElse(null);
-    }
+public interface CustomRegisteredClientRepository extends JpaRepository<RegisteredClientEntity, Long> {
+    Optional<RegisteredClientEntity> findByClientId(String clientId);
 }
---
+++ authorization-server/src/main/java/com/springbite/authorization_server/repositories/impls/RegisteredClientRepositoryImpl.java
@@ -0,0 +1,41 @@
+package com.springbite.authorization_server.repositories.impls;
+
+import com.springbite.authorization_server.entities.RegisteredClientEntity;
+import com.springbite.authorization_server.mappers.RegisteredClientMapper;
+import com.springbite.authorization_server.repositories.CustomRegisteredClientRepository;
+import org.springframework.security.oauth2.server.authorization.client.RegisteredClient;
+import org.springframework.security.oauth2.server.authorization.client.RegisteredClientRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class RegisteredClientRepositoryImpl implements RegisteredClientRepository {
+
+    private final CustomRegisteredClientRepository customRegisteredClientRepository;
+
+    private final RegisteredClientMapper registeredClientMapper;
+
+    public RegisteredClientRepositoryImpl(CustomRegisteredClientRepository registeredClientRepository, RegisteredClientMapper registeredClientMapper) {
+        this.customRegisteredClientRepository = registeredClientRepository;
+        this.registeredClientMapper = registeredClientMapper;
+    }
+
+    @Override
+    public void save(RegisteredClient registeredClient) {
+        RegisteredClientEntity entity = registeredClientMapper.toRegisteredClientEntity(registeredClient);
+        customRegisteredClientRepository.save(entity);
+    }
+
+    @Override
+    public RegisteredClient findById(String id) {
+        return customRegisteredClientRepository.findById(Long.valueOf(id))
+                .map(registeredClientMapper::toRegisteredClient)
+                .orElse(null);
+    }
+
+    @Override
+    public RegisteredClient findByClientId(String clientId) {
+        return customRegisteredClientRepository.findByClientId(clientId)
+                .map(registeredClientMapper::toRegisteredClient)
+                .orElse(null);
+    }
+}
