890e61dbe86439b79ed6f97edcf57f591a7ff566
AS <ahmedselo2005@gmail.com>
2024-08-17 20:03:19 +0300

    Add PasswordGenerator class for google accounts

---
+++ authorization-server/src/main/java/com/springbite/authorization_server/security/PasswordGenerator.java
@@ -0,0 +1,25 @@
+package com.springbite.authorization_server.security;
+
+import org.springframework.stereotype.Component;
+
+import java.security.SecureRandom;
+
+@Component
+public class PasswordGenerator {
+
+    private final String CHARACTERS =
+            "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()-_=+[]{}|;:,.<>?";
+
+    public String generateRandomPassword(int length) {
+        SecureRandom random = new SecureRandom();
+        StringBuilder password = new StringBuilder(length);
+
+        for (int i = 0; i < length; i++) {
+            int index = random.nextInt(CHARACTERS.length());
+            password.append(CHARACTERS.charAt(index));
+        }
+
+        return password.toString();
+    }
+
+}
