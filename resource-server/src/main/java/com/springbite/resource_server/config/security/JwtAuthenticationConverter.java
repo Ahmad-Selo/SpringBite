1894bd18db6111bef1bf7368088815e6a9b99a07
AS <ahmedselo2005@gmail.com>
2024-08-17 20:46:51 +0300

    A custom Jwt converter

---
+++ resource-server/src/main/java/com/springbite/resource_server/config/security/JwtAuthenticationConverter.java
@@ -0,0 +1,33 @@
+package com.springbite.resource_server.config.security;
+
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.stereotype.Component;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Component
+public class JwtAuthenticationConverter implements Converter<Jwt, CustomAuthentication> {
+
+    public JwtAuthenticationConverter() {
+    }
+
+    @Override
+    public CustomAuthentication convert(Jwt source) {
+        List<GrantedAuthority> authorities = extractAuthorities(source);
+
+        return new CustomAuthentication(source, authorities);
+    }
+
+    private List<GrantedAuthority> extractAuthorities(Jwt source) {
+        Collection<String> authorities = (Collection<String>) source.getClaims().get("authorities");
+
+        return authorities.stream()
+                .map(SimpleGrantedAuthority::new)
+                .collect(Collectors.toList());
+    }
+}
