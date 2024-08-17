696373fc7f1d262228b10516e19d3804e5f310cb
AS <ahmedselo2005@gmail.com>
2024-08-17 20:35:00 +0300

    Add filter for signup endpoint to ensure that a trusted client make the request

---
+++ authorization-server/src/main/java/com/springbite/authorization_server/filters/ClientAuthFilter.java
@@ -0,0 +1,65 @@
+package com.springbite.authorization_server.filters;
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.server.authorization.client.RegisteredClient;
+import org.springframework.security.oauth2.server.authorization.client.RegisteredClientRepository;
+
+import java.io.IOException;
+import java.util.Base64;
+
+public class ClientAuthFilter implements Filter {
+
+    private final RegisteredClientRepository registeredClientRepository;
+
+    private final PasswordEncoder passwordEncoder;
+
+    public ClientAuthFilter(RegisteredClientRepository registeredClientRepository, PasswordEncoder passwordEncoder) {
+        this.registeredClientRepository = registeredClientRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @Override
+    public void doFilter(
+            ServletRequest request,
+            ServletResponse response,
+            FilterChain chain
+    ) throws IOException, ServletException {
+        HttpServletRequest httpRequest = (HttpServletRequest) request;
+        HttpServletResponse httpResponse = (HttpServletResponse) response;
+
+        String uri = httpRequest.getRequestURI();
+
+        if (!uri.equals("/signup")) {
+            chain.doFilter(request, response);
+            return;
+        }
+
+        String authHeader = httpRequest.getHeader("Authorization");
+
+        if (authHeader == null || !authHeader.startsWith("Basic ")) {
+            httpResponse.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+            httpResponse.getWriter().write("Authorization header missing or invalid.");
+            return;
+        }
+
+        String base64Credentials = authHeader.substring(6);
+        byte[] decodedBytes = Base64.getDecoder().decode(base64Credentials);
+        String decodedCredentials = new String(decodedBytes);
+
+        String[] credentials = decodedCredentials.split(":", 2);
+        String clientId = credentials[0];
+        String clientSecret = credentials[1];
+
+        RegisteredClient registeredClient = registeredClientRepository.findByClientId(clientId);
+
+        if (registeredClient == null || !passwordEncoder.matches(clientSecret, registeredClient.getClientSecret())) {
+            httpResponse.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            httpResponse.getWriter().write("Invalid client credentials.");
+            return;
+        }
+        chain.doFilter(request, response);
+    }
+}
