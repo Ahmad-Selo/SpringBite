5a84c554df65d7aedc975a2e2db9bbac3758ed12
AS <ahmedselo2005@gmail.com>
2024-08-17 20:09:19 +0300

    UserDto and UserMapper with UserMapperTest class

---
+++ authorization-server/src/main/java/com/springbite/authorization_server/mappers/UserMapper.java
@@ -0,0 +1,39 @@
+package com.springbite.authorization_server.mappers;
+
+import com.springbite.authorization_server.models.SecurityUser;
+import com.springbite.authorization_server.models.User;
+import com.springbite.authorization_server.models.dtos.UserDto;
+import org.springframework.stereotype.Component;
+
+import static com.springbite.authorization_server.security.Role.ROLE_USER;
+
+@Component
+public class UserMapper {
+
+    public User toUser(UserDto dto) {
+        return new User(
+                dto.getUsername(),
+                dto.getPassword(),
+                dto.getFirstname(),
+                dto.getLastname(),
+                dto.getPhoneNumber(),
+                ROLE_USER,
+                true,
+                true
+        );
+    }
+
+    public UserDto toUserDto(User user) {
+        return new UserDto(
+                user.getUsername(),
+                user.getPassword(),
+                user.getFirstname(),
+                user.getLastname(),
+                user.getPhoneNumber()
+        );
+    }
+
+    public SecurityUser userToSecurityUser(User user) {
+        return new SecurityUser(user);
+    }
+}
