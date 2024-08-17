31cc19c1b594a4dcb405ca1b04965d99da1a0d3e
AS <ahmedselo2005@gmail.com>
2024-08-17 20:11:41 +0300

    Add SecurityConfig class

---
+++ resource-server/src/main/java/com/springbite/resource_server/config/SecurityConfig.java
@@ -0,0 +1,37 @@
+package com.springbite.resource_server.config;
+
+import com.springbite.resource_server.config.security.JwtAuthenticationConverter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Value("${keySetUri}")
+    private String keySetUri;
+
+    private final JwtAuthenticationConverter jwtAuthenticationConverter;
+
+    public SecurityConfig(JwtAuthenticationConverter jwtAuthenticationConverter) {
+        this.jwtAuthenticationConverter = jwtAuthenticationConverter;
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.oauth2ResourceServer(
+                o -> o.jwt(
+                        j -> j.jwkSetUri(keySetUri)
+                                .jwtAuthenticationConverter(jwtAuthenticationConverter)
+                )
+        );
+
+        http.authorizeHttpRequests(
+                c -> c.anyRequest().authenticated()
+        );
+
+        return  http.build();
+    }
+}
