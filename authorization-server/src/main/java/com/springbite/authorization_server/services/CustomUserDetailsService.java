d6a2c7b7a55556cd27069caadce1bb2e239e3c43
AS <ahmedselo2005@gmail.com>
2024-08-23 14:21:29 +0300

    Add a dot in the end of the message when the user not found.

--- authorization-server/src/main/java/com/springbite/authorization_server/services/CustomUserDetailsService.java
+++ authorization-server/src/main/java/com/springbite/authorization_server/services/CustomUserDetailsService.java
@@ -20,7 +20,8 @@ public class CustomUserDetailsService implements UserDetailsService {
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         User user = userRepository.findByUsername(username)
-                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+                .orElseThrow(() -> new UsernameNotFoundException("User not found."));
+
         return new SecurityUser(user);
     }
 }
