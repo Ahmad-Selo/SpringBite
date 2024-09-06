a84ddc0f571b4791355b4579e024941a141d27f9
AS <ahmedselo2005@gmail.com>
2024-09-06 19:09:29 +0300

    Use the /email prefix instead of /emails.

--- authorization-server/src/main/java/com/springbite/authorization_server/controllers/EmailController.java
+++ authorization-server/src/main/java/com/springbite/authorization_server/controllers/EmailController.java
@@ -11,7 +11,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/emails")
+@RequestMapping("/email")
 public class EmailController {
 
     private final EmailService emailService;
