8db86232b3c6ad88f217fde939576845ff22ae89
AS <ahmedselo2005@gmail.com>
2024-09-12 12:05:39 +0300

    Make the SecurityUserDeserializer class extends StdDeserializer instead of JsonDeserializer.

--- authorization-server/src/main/java/com/springbite/authorization_server/deserializers/SecurityUserDeserializer.java
+++ authorization-server/src/main/java/com/springbite/authorization_server/deserializers/SecurityUserDeserializer.java
@@ -3,15 +3,19 @@ package com.springbite.authorization_server.deserializers;
 import com.fasterxml.jackson.core.JacksonException;
 import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.databind.DeserializationContext;
-import com.fasterxml.jackson.databind.JsonDeserializer;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.deser.std.StdDeserializer;
 import com.springbite.authorization_server.models.SecurityUser;
 import com.springbite.authorization_server.models.User;
 
 import java.io.IOException;
 
-public class SecurityUserDeserializer extends JsonDeserializer<SecurityUser> {
+public class SecurityUserDeserializer extends StdDeserializer<SecurityUser> {
+
+    protected SecurityUserDeserializer() {
+        super(SecurityUser.class);
+    }
 
     @Override
     public SecurityUser deserialize(JsonParser p, DeserializationContext ctxt) throws IOException, JacksonException {
